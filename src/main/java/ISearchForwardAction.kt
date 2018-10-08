aad12d214175742ef570f01d151d76f55301de11
Takeshi Kunimi <takeshi.kunimi@umitron.com>
2018-10-08 19:52:33 +0900

    Add isearch-forward action!

---
+++ src/main/java/ISearchForwardAction.kt
@@ -0,0 +1,36 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAware
+
+class ISearchForwardAction : AnAction(), DumbAware {
+    init {
+        isEnabledInModalContext = true
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val dataContext = e.dataContext
+        val project = CommonDataKeys.PROJECT.getData(dataContext) ?: return
+        val editor = CommonDataKeys.EDITOR.getData(dataContext) ?: return
+
+        IncrementalSearchHandler().invoke(project, editor, false)
+    }
+
+    override fun update(event: AnActionEvent) {
+        val presentation = event.presentation
+        val dataContext = event.dataContext
+        val project = CommonDataKeys.PROJECT.getData(dataContext)
+        if (project == null) {
+            presentation.isEnabled = false
+            return
+        }
+
+        val editor = CommonDataKeys.EDITOR.getData(dataContext)
+        if (editor == null) {
+            presentation.isEnabled = false
+            return
+        }
+
+        presentation.isEnabled = true
+    }
+}
